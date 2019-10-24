7c1b87bab7216fb12ba4fc2ebe5e346bb6734e86
Heloise THERO <hthero@IFREMER.FR>
2019-10-24 12:44:11 +0200

    Functions with parameters and automated documentation!

--- R/useful_functions.R
+++ R/useful_functions.R
@@ -13,15 +13,38 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+#' Print "hello word"
+#'
+#' @examples hello()
 hello <- function() {
   print("Hello, world!")
 }
 
+#' Plot the mpg by wt of the mtcars data using the base package
+#'
+#' @examples show_mtcars()
 show_mtcars <- function() {
   data(mtcars)
   plot(mtcars$mpg, mtcars$wt)
 }
 
+#' Plot the mpg by wt of the mtcars data using the ggplot2 package
+#'
+#' @examples show_mtcars2()
 show_mtcars2 <- function() {
   qplot(mpg, wt, data = mtcars)
 }
+
+
+#' Add together two numbers
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}
+#' @examples
+#' add(1, 1)
+#' add(10, -2)
+add <- function(x, y) {
+  x + y
+}
+
