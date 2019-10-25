7ce41b2beb8305ca4db69e22a3727c3c41832cdf
Heloise THERO <hthero@IFREMER.FR>
2019-10-25 17:41:19 +0200

    Add visible binding to some variables after a note from devtools::check()

--- R/test.R
+++ R/test.R
@@ -3,5 +3,5 @@
 #' @examples plot_test_ggplot2_figure()
 #' @export
 plot_test_ggplot2_figure <- function() {
-  ggplot2::qplot(mpg, wt, data = mtcars)
+  ggplot2::qplot(datasets::mtcars$mpg, datasets::mtcars$wt)
 }
