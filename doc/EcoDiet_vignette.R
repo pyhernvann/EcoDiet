b259ee49b6020eaefab40b7e0f7f536344e4d845
Heloise THERO <hthero@IFREMER.FR>
2019-11-13 17:46:06 +0100

    Force the vignettes inside the package

---
+++ doc/EcoDiet_vignette.R
@@ -0,0 +1,41 @@
+## ---- include = FALSE----------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ---- eval = FALSE-------------------------------------------------------
+#  install.packages("devtools")
+#  library(devtools)
+#  
+
+## ---- eval = FALSE-------------------------------------------------------
+#  devtools::install_github("heloisethero/EcoDiet")
+
+## ------------------------------------------------------------------------
+library(TestEcoDiet)
+
+
+## ------------------------------------------------------------------------
+example_stomach_path <- system.file("extdata", "ecodiet_example_stomach.csv",
+                                    package = "TestEcoDiet")
+example_isotope_path <- system.file("extdata", "ecodiet_example_isotope.csv",
+                                    package = "TestEcoDiet")
+data <- load_data(example_stomach_path, example_isotope_path)
+
+## ---- eval = FALSE-------------------------------------------------------
+#  data <- load_data("my_EcoDiet_analysis/my_stomach_daya.csv",
+#                    "my_EcoDiet_analysis/my_isotope_daya.csv")
+
+## ------------------------------------------------------------------------
+data <- list(stomach_data = ecodiet_example_stomach, isotope_data = ecodiet_example_isotope)
+
+
+## ------------------------------------------------------------------------
+print(ecodiet_example_stomach)
+
+
+## ------------------------------------------------------------------------
+head(ecodiet_example_isotope)
+
+
