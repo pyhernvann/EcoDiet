6201faf842ebb7d06d5ceed89256fbbd16cf78e3
heloisethero <thero.heloise@gmail.com>
2019-11-20 18:02:09 +0100

    Create the function to load a data list, ready to be given to the jags function. This function is based on the running_EcoDiet and EcoDiet_function script from the original Pierre-Yves' scripts.

--- R/load_data.R
+++ R/load_data.R
@@ -1,15 +1,124 @@
-#' Load the stomachal and the isotopic data to feed the EcoDiet model.
+#' Load the raw data and process it to feed the EcoDiet model
 #'
-#' @param stomach_data_path the path to access the stomachal data
-#' @param isotope_data_path the path to access the isotopic data
-#' @return a list containing the stomachal and isotopic data
+#' @param raw_data_list the list containing the different raw data objects
+#' 
+#' @return a list of processed data, ready to be run by the EcoDiet model
+#' 
+#' @examples
+#' data <- load_data(ecodiet_example)
 #'
 #' @export
 
-load_data <- function(stomach_data_path, isotope_data_path){
+load_data <- function(raw_data_list){
   
-  stomach_data <- utils::read.csv(stomach_data_path)
-  isotope_data <- utils::read.csv(isotope_data_path)
+  topo_run <- raw_data_list$topo_run
+  topo_run <- topo_run[, order(colnames(topo_run))]
+  topo_run <- topo_run[order(rownames(topo_run)), ]
+  topo_run <- as.matrix(topo_run)
   
-  return(list(stomach_data = stomach_data, isotope_data = isotope_data))
+  # Constructs the index from the binary web matrix
+  
+  names_grp <- rownames(topo_run)
+  nb_grp <- nrow(topo_run)
+  nb_sources  <- colSums(topo_run)
+  
+  id_base <- which(colSums(topo_run) == 0)
+  nb_base <- length(id_base)
+  id_cons <- which(colSums(topo_run) != 0)
+  nb_cons <- length(id_cons)
+  
+  id_source <- t(sapply(1:nb_cons,function(i){ 
+    output <- rep(NA,max(nb_sources)) 
+    output[1:nb_sources[id_cons][i]] <- which(topo_run[,id_cons[i]]==1) 
+    output
+  }))
+  
+  # stomachal data
+  
+  nb_stom_SCA_data <- raw_data_list$nb_stom_SCA_data
+  nb_stom_SCA_data <- nb_stom_SCA_data[, order(colnames(nb_stom_SCA_data))]
+  
+  id_with_sca <- which(nb_stom_SCA_data[1, id_cons] != 0)
+  id_without_sca <- seq(1, nb_cons)[-id_with_sca]  
+  nb_with_sca <- length(id_with_sca)
+  
+  
+  SCA_data <- raw_data_list$SCA_data
+  SCA_data <- SCA_data[, order(colnames(SCA_data))]
+  SCA_data <- SCA_data[order(rownames(SCA_data)), ]
+  SCA_input <- as.matrix(SCA_data)
+  
+  # parameters for the isotopic analysis
+  
+  el_conc <- as.matrix(raw_data_list$element_conc_data)
+  
+  tdf <- as.matrix(raw_data_list$mean_tdf)
+  
+  # SIA data preprocessing
+  
+  SIA_data <- raw_data_list$isotope_data
+  SIA_data <- SIA_data[order(SIA_data$Group), ] 
+  
+  nb_elements <- ncol(SIA_data) - 1
+  nb_max_spl <- max(table(SIA_data$Group))
+  
+  SIA_input <- array(NA, dim=c(nb_elements, nb_grp, nb_max_spl))
+  colnames(SIA_input) <- names_grp
+  
+  for (el in 1:nb_elements){
+    for (grp in 1:nb_grp){
+      SIA_input[el, grp, ] <- SIA_data[SIA_data$Group==names_grp[grp], el + 1]
+    }
+  }
+  
+  nb_samples <- apply(ifelse(!is.na(SIA_input), 1, 0), seq(1, nb_elements), sum)
+  
+  # parameters for the priors
+  
+  literature_data <- raw_data_list$literature_data
+  
+  Pedigree_literature_data <- raw_data_list$pedigree_literature_data
+  Ped <- as.matrix(Pedigree_literature_data)
+  Ped <- Ped[ ,order(colnames(Ped))]
+  
+  g_slope_param <- 0.5
+  g_slope_param <- as.vector(g_slope_param)
+  CV_calc <- 1 - Ped * g_slope_param[1]
+  
+  n_lit_param <- 30
+  n_lit <- as.matrix(n_lit_param)
+  
+  literature_data <- raw_data_list$literature_data
+  priors_lit <- as.matrix(literature_data)
+  priors_lit <- priors_lit[,order(colnames(priors_lit))]
+  priors_lit <- priors_lit[order(rownames(priors_lit)),]
+  
+  ED_conf <- 3
+  switch_conf_2_3 <- ifelse(ED_conf%in%c(2,3),1,0)
+  switch_conf_3 <- ifelse(ED_conf==3,1,0)
+  
+  data_list <- list(
+    y     = SIA_input	,
+    o     = SCA_input,
+    nb    = nb_base,
+    nc   	= nb_cons,
+    ns    = nb_sources,
+    n_sia = nb_samples,
+    ne    = nb_elements,
+    DELTA = tdf ,
+    q     = el_conc,
+    ZEROS = matrix(0, nrow=nb_elements, ncol=nb_cons),
+    ID    = diag(nb_elements),
+    ic    = id_cons,
+    is    = id_source,
+    n_sca = nb_stom_SCA_data,
+    g     = Ped,
+    CVs   = CV_calc,
+    alpha_lit = priors_lit,
+    n_lit = n_lit,
+    switch_conf_2_3 = switch_conf_2_3,
+    switch_conf_3 = switch_conf_3
+  )
+  
+  return(data_list)
 }
