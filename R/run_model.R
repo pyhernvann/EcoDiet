bd676807ef23f23f5e58b6d997b607e8ae6bd407
heloisethero <thero.heloise@gmail.com>
2019-11-28 10:42:38 +0100

    Create the run_model_rjags function to use rjags instead of R2jags

--- R/run_model.R
+++ R/run_model.R
@@ -2,7 +2,8 @@
 #'
 #' @param data the list containing the different loaded and preprocessed data objects
 #' @param inits the list containing the initial values of the variables
-#' @param model_string the string containing the definition of the EcoDiet model
+#' @param model_file the file containing the definition of the EcoDiet model
+#' @param n_iter the number of iterations for the model to be run
 #' 
 #' @return an mcmc.list containing the variables to store, i.e., the pi and lambda variables
 #' 
@@ -30,6 +31,37 @@ run_model <- function(data, inits, model_file, n_iter = 1e+03){
     n.burnin = n_burnin, n.thin = n_thin)
   
   mcmc_output <- coda::as.mcmc(mcmc_output)
+  
+  end_time <- Sys.time()
+  cat(end_time - start_time)
+  
+  return(mcmc_output)
+}
+
+#' Run the MCMC chains for the EcoDiet model with the rjags package
+#'
+#' @export
+
+run_model_rjags <- function(data, inits, model_file, n_iter = 1e+03){
+  
+  start_time <- Sys.time()
+  
+  n_chains <- 3
+  n_burnin <- floor(n_iter/2)
+  n_thin <- max(1, floor((n_iter - n_burnin)/1000))
+  
+  jags <- rjags::jags.model(
+    data = data, 
+    inits = inits,
+    file = model_file,
+    n.chains = n_chains,
+    n.adapt = n_burnin)
+  
+  mcmc_output <- rjags::coda.samples(
+    model = jags,
+    variable.name = c("LAMBDA", "PI"),
+    n.iter = (n_iter - n_burnin),
+    thin = n_thin)
   
   end_time <- Sys.time()
   cat(end_time - start_time)
