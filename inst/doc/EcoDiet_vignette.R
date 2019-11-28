aeaa20dc8a9cd9dbbf4f23939b2df180d1a8e47b
heloisethero <thero.heloise@gmail.com>
2019-11-28 11:13:40 +0100

    Put the information about rescaling the stomachal data in another vignette.

--- inst/doc/EcoDiet_vignette.R
+++ inst/doc/EcoDiet_vignette.R
@@ -4,14 +4,6 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
-## ---- eval = FALSE-------------------------------------------------------
-#  install.packages("devtools")
-#  library(devtools)
-#  
-
-## ---- eval = FALSE-------------------------------------------------------
-#  devtools::install_github("heloisethero/EcoDiet", auth_token='c570f1b55d68a8ae0fb38428155e19a426814d15')
-
 ## ------------------------------------------------------------------------
 library(EcoDiet)
 
@@ -22,16 +14,6 @@ names(ecodiet_example)
 
 ## ------------------------------------------------------------------------
 preprocessed_data <- load_data(ecodiet_example)
-
-
-## ------------------------------------------------------------------------
-preprocessed_data$o
-preprocessed_data$n_sca
-
-
-## ------------------------------------------------------------------------
-preprocessed_data$o <- rescale_stomach_data(preprocessed_data$o, preprocessed_data$n_sca)
-preprocessed_data$o
 
 
 ## ------------------------------------------------------------------------
