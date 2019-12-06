0a5099fc654fbf3f7e67138296dc83557f3d750f
heloisethero <thero.heloise@gmail.com>
2019-12-06 14:21:35 +0100

    Add an automatic Gelman-Rubin convergence diagnostic to the run_model function.

--- R/run_model.R
+++ R/run_model.R
@@ -44,5 +44,23 @@ run_model <- function(model_file, data, inits = NULL, n_iter = 1e+03, n_chains =
   cat("The time that the model took to run is:") 
   print(end_time - start_time)
   
+  gelman <- coda::gelman.diag(mcmc_output, multivariate = FALSE)$psrf[, 1]
+  variable_number <- length(gelman)
+  gelman <- gelman[which(!is.nan(gelman))]
+  variable_number_over_1.1 <- length(gelman[which(gelman > 1.1)])
+  variable_number_over_1.05 <- length(gelman[which(gelman > 1.05)])
+  
+  if (variable_number_over_1.1 > 0){
+    message("\n          /!\\   /!\\   BIG CONVERGENCE PROBLEM   /!\\   /!\\ \n")
+    message("Out of the ", variable_number,  " variables, ",
+            variable_number_over_1.1, " variables have a Gelman-Rubin diagnostic > 1.1.")
+  } else if (variable_number_over_1.05 > 0){
+    message("\n             /!\\   /!\\   CONVERGENCE PROBLEM   /!\\   /!\\ \n")
+    message("Out of the ", variable_number,  " variables, ",
+            variable_number_over_1.05, " variables have a Gelman-Rubin diagnostic > 1.05.")
+  }
+  message("\nYou should increase the number of iterations of your model with the `n_iter` argument:")
+  message("> mcmc_output <- run_model(textConnection(model_string), data, n_iter = 1e+06)\n")
+  
   return(mcmc_output)
 }
