0e5549639adcb56b2881d851e45953203bb2d093
heloisethero <thero.heloise@gmail.com>
2020-01-10 16:28:15 +0100

    Git the doc folder so the user can access the html vignettes without having to use the "build_vignettes" argument.

---
+++ inst/doc/model_warnings.R
@@ -0,0 +1,25 @@
+## ---- eval = FALSE------------------------------------------------------------
+#  load("convergence_diagnostic.Rdata")
+#  View(gelman)
+
+## -----------------------------------------------------------------------------
+library(EcoDiet)
+example_stomach_data_path <- system.file("extdata", "example_stomach_data.csv",
+                                    package = "EcoDiet")
+example_isotope_data_path <- system.file("extdata", "example_isotope_data.csv",
+                                    package = "EcoDiet")
+data <- preprocess_data(isotope_data = read.csv(example_isotope_data_path),
+                        trophic_enrichment_factor = c(0.8, 3.4),
+                        literature_prior = FALSE,
+                        stomach_data = read.csv(example_stomach_data_path))
+model_string <- write_model(literature_prior = FALSE)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  mcmc_output <- run_model(textConnection(model_string), data, nb_iter = 1e+08)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  mcmc_output <- run_model(textConnection(model_string), data, nb_iter = 1e+06, nb_adapt = 1e+04)
+
+## -----------------------------------------------------------------------------
+devtools::session_info()
+
